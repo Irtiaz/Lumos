cb9ebcbab256b85728ee1edb99340f8b583623f2
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-10-16 16:29:00 +0600

    Added skelephoton

---
+++ skelephoton/skelephoton.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var SPEED: float = 3000
+@export var player: CharacterBody2D
+@onready var nav_agent := $CollisionShape2D.get_node('NavigationAgent2D') as NavigationAgent2D
+
+func _ready() -> void:
+	nav_agent.target_position = player.global_position
+	#nav_agent.target_position = get_global_mouse_position()
+
+func _process(delta: float) -> void:
+	nav_agent.target_position = player.global_position
+	$AnimatedSprite2D.flip_h = velocity.x < 0
+
+func _physics_process(delta: float) -> void:
+	var dir = to_local(nav_agent.get_next_path_position()) - to_local($CollisionShape2D.global_position)
+	velocity = dir.normalized() * SPEED * delta
+	move_and_slide()
