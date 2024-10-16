a17d0745dfda3a6b978150381b350f85ead93fbb
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-10-16 20:28:31 +0600

    enemy wakes, gets visible in light only

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -4,11 +4,15 @@ extends CharacterBody2D
 @export var player: CharacterBody2D
 @onready var nav_agent := $CollisionShape2D.get_node('NavigationAgent2D') as NavigationAgent2D
 
+var is_awake = false
+
 func _ready() -> void:
 	nav_agent.target_position = player.global_position
 	#nav_agent.target_position = get_global_mouse_position()
 
 func _process(delta: float) -> void:
+	if !is_awake:
+		return
 	nav_agent.target_position = player.global_position
 	$AnimatedSprite2D.flip_h = velocity.x < 0
 	
@@ -16,6 +20,16 @@ func _process(delta: float) -> void:
 		queue_free()
 
 func _physics_process(delta: float) -> void:
+	if !is_awake:
+		return
 	var dir = to_local(nav_agent.get_next_path_position()) - to_local($CollisionShape2D.global_position)
 	velocity = dir.normalized() * SPEED * delta
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	is_awake = true
+	show()
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	hide()
