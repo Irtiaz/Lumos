63c60ce7549443bf23137a714dc2b83ed4006da0
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-10-16 19:41:55 +0600

    Skelephotons will always spawn at free cell

--- World/world.gd
+++ World/world.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var skelephotonScene = preload("res://skelephoton/skelephoton.tscn")
+@onready var tilemap = $TileMapLayer as TileMapLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,9 +12,36 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	if (Input.is_action_just_pressed("spawn_skelephoton")):
+		var cell_coord = tilemap.local_to_map(get_local_mouse_position())
+		var safe_coord = find_nearest_cell(cell_coord)
+		
+		if safe_coord == Vector2i(-1, -1): return
+		
 		var skelephoton = skelephotonScene.instantiate()
 		skelephoton.player = $Player
-		skelephoton.position = get_global_mouse_position()
+		skelephoton.position = tilemap.map_to_local(safe_coord)
 		add_child(skelephoton)
 	
 	pass
+
+func is_safe(cell_coord: Vector2i) -> bool:
+	return tilemap.get_cell_atlas_coords(cell_coord) == Vector2i(1, 4)
+
+func find_nearest_cell(cell_coord: Vector2i) -> Vector2i:
+	const list: Array[Vector2i] = [
+		Vector2i(0, 0),
+		Vector2i(-1, 0),
+		Vector2i(1, 0),
+		Vector2i(0, -1),
+		Vector2i(0, 1),
+		Vector2i(1, 1),
+		Vector2i(1, -1),
+		Vector2i(-1, 1),
+		Vector2i(-1, -1)
+	]
+	
+	for offset in list:
+		var current_coord = cell_coord + offset
+		if is_safe(current_coord): return current_coord
+	return Vector2i(-1, -1)
+	
