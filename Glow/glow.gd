d1f06f330b5854fc585b405ec37d31ecc712bf3a
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-10-16 20:19:48 +0600

    enemy die bug fixed

--- Glow/glow.gd
+++ Glow/glow.gd
@@ -12,7 +12,6 @@ const factor := 0.75
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(radius)
 	$Area2D/CollisionShape2D.shape = CircleShape2D.new()
 	$Area2D/CollisionShape2D.shape.set_radius(radius * factor)
 	pass # Replace with function body.
